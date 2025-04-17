6b0fd80d6ed67766fac1197de0442d0d9b2a78a8
Moamen771 <memoaser86@gmail.com>
2025-04-17 14:33:53 +0200

    edit colors name

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,29 +1,8 @@
 import 'dart:ui';
 
 class AppColors {
-  static final color11 = Color(0xFFF1EFEC);
-  static final color12 = Color(0xFFD4C9BE);
-  static final color13 = Color(0xFF123458);
-  static final color14 = Color(0xFF030303);
-
-  //
-
-  static final color21 = Color(0xFFF2EFE7);
-  static final color22 = Color(0xFF9ACBD0);
-  static final color23 = Color(0xFF48A6A7);
-  static final color24 = Color(0xFF006A71);
-
-  //
-
-  static final color31 = Color(0xFFF8FAFC);
-  static final color32 = Color(0xFFD9EAFD);
-  static final color33 = Color(0xFFBCCCDC);
-  static final color34 = Color(0xFF9AA6B2);
-
-  //
-
-  static final color41 = Color(0xFF003C43);
-  static final color42 = Color(0xFF135D66);
-  static final color43 = Color(0xFF77B0AA);
-  static final color44 = Color(0xFFE3FEF7);
+  static const lighterColor = Color(0xFFF8FAFC);
+  static const lightColor = Color(0xFFD9EAFD);
+  static const darkColor = Color(0xFFBCCCDC);
+  static const darkerColor = Color(0xFF9AA6B2);
 }
