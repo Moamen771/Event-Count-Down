e557b3fbe34a94ba0a25ddc95e984fd7ad9f520f
Moamen771 <memoaser86@gmail.com>
2025-05-07 00:18:02 +0300

    refactor splash screen

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,12 +1,12 @@
 import 'dart:ui';
 
 class AppColors {
-  static const lighterColor = Color(0xFFF8FAFC);
-  static const lightColor = Color(0xFFD9EAFD);
-  static const darkColor = Color(0xFFBCCCDC);
-  static const darkerColor = Color(0xFF9AA6B2);
+  static const lighterColor = Color(0xFFF9F7F7);
+  static const lightColor = Color(0xFFDBE2EF);
+  static const darkColor = Color(0xFF3F72AF);
+  static const darkerColor = Color(0xFF112D4E);
   static const primaryColor = Color(0xFF4052d6);
-  
+
   static const whiteColor = Color(0xFFFFFFFF);
   static const blackColor = Color(0xFF000000);
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
-import 'package:eventcountdown/screens/new_event_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+
+import '../constants/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,38 +13,30 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    // Future.delayed(
-    //   Duration(milliseconds: 2500),
-    //   () => Navigator.pushReplacement(
-    //     context,
-    //     MaterialPageRoute(
-    //       builder: (context) => NewEventScreen(),
-    //     ),
-    //   ),
-    // );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.lighterColor,
       body: Stack(
         children: [
           //ToDo: Logo animation
           Center(
             child: Image.asset(
-              'assets/images/splash/splash_even.png',
+              'assets/images/splash/splash-logo-even.png',
             ).animate().slideY(
-                  begin: 5,
+                  begin: 2,
                   duration: const Duration(seconds: 2),
                   curve: Curves.elasticInOut,
                 ),
           ),
           Center(
             child: Image.asset(
-              'assets/images/splash/splash_t.png',
+              'assets/images/splash/splash-logo-t.png',
             ).animate().slideY(
-                  begin: -5,
+                  begin: -2,
                   duration: const Duration(seconds: 2),
                   curve: Curves.elasticInOut,
                 ),
