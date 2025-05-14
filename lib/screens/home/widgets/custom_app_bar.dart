0a06ad9b6891d025466482a0e92e004db3a81f6a
Moamen771 <memoaser86@gmail.com>
2025-05-15 00:31:35 +0300

    make custom_app_bar has customizable title

--- lib/screens/home/widgets/custom_app_bar.dart
+++ lib/screens/home/widgets/custom_app_bar.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import '../../../utils/app_colors.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         Text(
-          'Event Countdown',
-          style: TextStyle(
+          title,
+          style: const TextStyle(
             color: AppColors.darkerColor,
             fontSize: 20,
             fontWeight: FontWeight.bold,
