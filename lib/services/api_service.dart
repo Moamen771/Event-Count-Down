f9b816ff42927ac4a15c99de0f32433110fe9b37
Moamen771 <memoaser86@gmail.com>
2025-05-15 00:32:33 +0300

    refactor loadEvents api fun

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,15 +6,19 @@ class ApiService {
   final apiKey =
       '35f58eb5c776d9be4f7e82d5bfb7983babf3be58ee3147991594d90a763edf5b';
 
-  Future<List<OnlineEvent>> loadEvents(String title) async {
+  Future<List<OnlineEvent>?> loadEvents(String title) async {
     final url =
         'https://serpapi.com/search?engine=google&q=Events+in+$title&gl=eg&api_key=$apiKey';
     final response = await dio.get(url);
+    if (response.data == null ||
+        response.data['organic_results'] == null ||
+        response.data['organic_results'] is! List) {
+      return null;
+    }
     final eventsList = <OnlineEvent>[];
     for (var item in response.data['organic_results']) {
       eventsList.add(OnlineEvent.fromJson(item));
     }
-    // ['title'] ['snippet'] ['favicon'] ['link']
 
     return eventsList;
   }
@@ -24,3 +28,5 @@ class ApiService {
 //   final api = ApiService();
 //   api.loadEvents('cairo');
 // }
+
+// https://serpapi.com/search?engine=google&q=Events+in+$title&gl=eg&api_key=$apiKey'
