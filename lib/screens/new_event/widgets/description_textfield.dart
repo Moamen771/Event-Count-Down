4bde9470410d438b9208a1b4b00949540d979022
Noureddine <nooraldeenmohamed99@gmail.com>
2025-05-15 20:26:55 +0300

    edit_shape_of_description_textfield

--- lib/screens/new_event/widgets/description_textfield.dart
+++ lib/screens/new_event/widgets/description_textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../../../utils/app_colors.dart';
 
 class DescriptionTextField extends StatelessWidget {
@@ -15,47 +16,68 @@ class DescriptionTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         labelText == null
             ? const SizedBox()
-            : Text(
-                labelText ?? "",
-                style: const TextStyle(
-                  color: AppColors.blackColor,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
+            : RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: labelText ?? "",
+                      style: const TextStyle(
+                        color: AppColors.darkColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "*",
+                      style: TextStyle(
+                        color: Colors.red.withOpacity(0.7),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-        Container(
-          width: double.infinity,
-          height: screenHeight * (1 / 5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(color: AppColors.darkerColor, width: 2),
-          ),
-          child: TextFormField(
-            cursorColor: AppColors.primaryColor,
-            controller: controller,
-            validator: validator,
-
-            maxLines: null,
-            keyboardType: TextInputType.multiline,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              enabledBorder: InputBorder.none,
-              focusedBorder: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: 16, horizontal: 12), // Adjust padding
+        TextFormField(
+          cursorColor: AppColors.darkColor,
+          controller: controller,
+          validator: validator,
+          cursorHeight: 25,
+          maxLines: 4,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: AppColors.darkColor),
             ),
-            style: const TextStyle(
-              color: AppColors.blackColor,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-            ), // Optional: Adjust text style
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: AppColors.darkColor),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide:
+                  const BorderSide(color: AppColors.darkColor, width: 1.5),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.red, width: 1.5),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.red, width: 1.5),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+          ),
+          style: const TextStyle(
+            color: AppColors.blackColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
           ),
         ),
       ],
