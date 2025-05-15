cafa482bbe83e4112ade11c89e46d4519ff54c58
Moamen771 <memoaser86@gmail.com>
2025-05-16 01:19:04 +0300

    init local notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eventcountdown/services/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app.dart';
@@ -5,6 +6,7 @@ import 'app.dart';
 void main() {
   WidgetsBinding widgetsFlutterBinding =
       WidgetsFlutterBinding.ensureInitialized();
+  LocalNotificationsService.init();
   FlutterNativeSplash.preserve(
     widgetsBinding: widgetsFlutterBinding,
   );
